74d785f5c6af67ea5473544ade287a072ef762f8
aysha-12 <munniaysha555@gmail.com>
2025-06-15 21:23:44 +0600

    add

--- src/Hero/slider/Slider.jsx
+++ src/Hero/slider/Slider.jsx
@@ -10,7 +10,7 @@ const Slider = () => {
   }}>
                 <div className='flex justify-start  px-5 md:px-25'>
                     <div className='w-full md:w-1/2 '>
-                    <h1 className='text-3xl md:text-8xl text-white'>Rediscover Youth, <span className='text-[#00c3b9]'> Redefine Life.</span></h1>
+                    <h1 className='text-6xl md:text-8xl text-white'>Rediscover Youth, <span className='text-[#00c3b9]'> Redefine Life.</span></h1>
                     <p className='text-neutral-200  mt-5'>Embrace a renewed sense of vitality and confidence with advanced anti-aging therapies. At Relive, we help you unlock the best version of yourself inside and out.</p>
                    <button className="btn bg-[#00c3b9]  p-6 mt-5 text-white rounded-full text-[16px] ">Contact Us</button>
                      
